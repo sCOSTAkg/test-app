47124c05e5a3e56cfc007fdc73c69d89a06b58a9
sCOSTAkg <abakirsallahadin@gmail.com>
2025-10-22 17:54:06 +0600

    feat: add Spring Boot main application class

---
+++ src/main/java/com/example/testapp/Application.java
@@ -0,0 +1,11 @@
+package com.example.testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@cublic class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
